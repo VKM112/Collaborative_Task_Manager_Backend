e3de1298f0a16ed6c1881ed75dcad5f7e4821764
Mohan <vkmohan112@gmail.com>
2026-01-19 09:35:17 +0530

    New update in prisma.ts

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 import { loadEnv } from './loadEnv'
 
 loadEnv()
@@ -9,7 +10,16 @@ if (!connectionString) {
   throw new Error('DATABASE_URL is required to configure Prisma')
 }
 
-const adapter = new PrismaPg({ connectionString })
+const sslRequested =
+  process.env.DATABASE_SSL === 'true' || process.env.DATABASE_SSL === '1'
+const sslInConnectionString = /[?&]ssl(mode)?=/i.test(connectionString)
+
+const pool = new Pool({
+  connectionString,
+  ssl: sslRequested && !sslInConnectionString ? { rejectUnauthorized: false } : undefined,
+})
+
+const adapter = new PrismaPg(pool)
 const prisma = new PrismaClient({ adapter })
 
 export default prisma
